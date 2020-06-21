7eddc698d0e565b0dc7f5fd3d24926aec02d0d07
Aishah <aissshah@outlook.com>
2020-06-21 17:27:35 +0100

    Rearrange js file, fix functions

--- brains.js
+++ brains.js
@@ -1,29 +1,43 @@
-//Event Listeners
-document.addEventListener("keypress", keyBoard);
-document.getElementById("zero").addEventListener("click", makeItWork(0));
-document.getElementById("one").addEventListener("click", makeItWork(1));
-document.getElementById("two").addEventListener("click", makeItWork(2));
-document.getElementById("three").addEventListener("click", makeItWork(3));
-document.getElementById("four").addEventListener("click", makeItWork(4));
-document.getElementById("five").addEventListener("click", makeItWork(5));
-document.getElementById("six").addEventListener("click", makeItWork(6));
-document.getElementById("seven").addEventListener("click", makeItWork(7));
-document.getElementById("eight").addEventListener("click", makeItWork(8));
-document.getElementById("nine").addEventListener("click", makeItWork(9));
-//document.getElementById("delete").addEventListener("click", makeItWork);
-document.getElementById("divide").addEventListener("click", makeItWork(/));
-document.getElementById("multiply").addEventListener("click", makeItWork(*));
-document.getElementById("minus").addEventListener("click", makeItWork(-));
-document.getElementById("add").addEventListener("click", makeItWork(+));
+
+//Dummy variables
+let disp = "";
+let screen = document.getElementById("display");
 
 //Functions
 const makeItWork = val => {
-
+    disp += val;
+    screen.textContent = disp;
 };
+
+const resetDisplay = () => {
+    screen.textContent = "0";
+    disp = "";
+}
 
 //potential problem, backspace button only has keycode, not charcode
 //check keypress works for backspace button, otherwise change to keydown
-const keyBoard = (event) => {
+//const keyBoard = (event) => {
 
-};
+//};
+
+//Event Listeners
+
+    //document.addEventListener("keypress", keyBoard);
+    //document.getElementById("zero").onclick = () => makeItWork("0");
+    document.getElementById("zero").addEventListener("click", () => makeItWork("0"));
+    document.getElementById("one").addEventListener("click", () => makeItWork("1"));
+    document.getElementById("two").addEventListener("click", () => makeItWork("2"));
+    document.getElementById("three").addEventListener("click", () => makeItWork("3"));
+    document.getElementById("four").addEventListener("click", () => makeItWork("4"));
+    document.getElementById("five").addEventListener("click", () => makeItWork("5"));
+    document.getElementById("six").addEventListener("click", () => makeItWork("6"));
+    document.getElementById("seven").addEventListener("click", () => makeItWork("7"));
+    document.getElementById("eight").addEventListener("click", () => makeItWork("8"));
+    document.getElementById("nine").addEventListener("click", () => makeItWork("9"));
+    document.getElementById("delete").addEventListener("click", resetDisplay);
+    document.getElementById("divide").addEventListener("click", () => makeItWork("/"));
+    document.getElementById("multiply").addEventListener("click", () => makeItWork("*"));
+    document.getElementById("minus").addEventListener("click", () => makeItWork("-"));
+    document.getElementById("add").addEventListener("click", () => makeItWork("+"));
+
 
