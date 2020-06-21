14987757d8347e7e9de6134d1366537b12b5d090
Aishah <aissshah@outlook.com>
2020-06-21 16:26:21 +0100

    Add click functionality to buttons using event listeners

--- brains.js
+++ brains.js
@@ -1,18 +1,29 @@
 //Event Listeners
-//document.getElementById("zero").addEventListener("click", makeItWork);
-//document.getElementById("zero").addEventListener("click", makeItWork);
+document.addEventListener("keypress", keyBoard);
+document.getElementById("zero").addEventListener("click", makeItWork(0));
+document.getElementById("one").addEventListener("click", makeItWork(1));
+document.getElementById("two").addEventListener("click", makeItWork(2));
+document.getElementById("three").addEventListener("click", makeItWork(3));
+document.getElementById("four").addEventListener("click", makeItWork(4));
+document.getElementById("five").addEventListener("click", makeItWork(5));
+document.getElementById("six").addEventListener("click", makeItWork(6));
+document.getElementById("seven").addEventListener("click", makeItWork(7));
+document.getElementById("eight").addEventListener("click", makeItWork(8));
+document.getElementById("nine").addEventListener("click", makeItWork(9));
+//document.getElementById("delete").addEventListener("click", makeItWork);
+document.getElementById("divide").addEventListener("click", makeItWork(/));
+document.getElementById("multiply").addEventListener("click", makeItWork(*));
+document.getElementById("minus").addEventListener("click", makeItWork(-));
+document.getElementById("add").addEventListener("click", makeItWork(+));
 
 //Functions
-const makeItWork = () => {
+const makeItWork = val => {
 
 };
 
-function uniCharCode(event) {
-    var char = event.which || event.keyCode;
-    document.getElementById("demo").innerHTML = "Unicode CHARACTER code: " + char;
-  }
-  
-  function uniKeyCode(event) {
-    var key = event.keyCode;
-    document.getElementById("demo2").innerHTML = "Unicode KEY code: " + key;
-  }
+//potential problem, backspace button only has keycode, not charcode
+//check keypress works for backspace button, otherwise change to keydown
+const keyBoard = (event) => {
+
+};
+
